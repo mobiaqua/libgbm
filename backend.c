58ea48b378987697aa30d19b2bd87ffa2af16b5b
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2019-12-27 17:51:39 +0100

    synced with latest gbm lib

--- backend.c
+++ backend.c
@@ -37,32 +37,61 @@
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
 
-/* a more clever scheme would be to discover backends in a certain
- * directory..
- */
-static const char *backends[] = {
-      "gbm_pvr.so",
+extern const struct gbm_backend gbm_pvr_backend;
+
+struct backend_desc {
+   const char *name;
+   const struct gbm_backend *builtin;
+};
+
+static const struct backend_desc backends[] = {
+//   { "gbm_pvr.so", &gbm_pvr_backend },
+   { "gbm_pvr.so", NULL },
 };
 
 static const void *
-load_backend(const char *name)
+load_backend(const struct backend_desc *backend)
 {
+   const void *init = NULL;
    char path[PATH_MAX];
    void *module;
    const char *entrypoint = "gbm_backend";
 
-   if (name[0] != '/')
-      snprintf(path, sizeof path, MODULEDIR "/%s", name);
-   else
-      snprintf(path, sizeof path, "%s", name);
-
-   module = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
-   if (!module) {
-      fprintf(stderr, "failed to load module: %s\n", dlerror());
+   if (backend == NULL)
       return NULL;
+
+   if (backend->builtin) {
+      init = backend->builtin;
+   } else {
+      if (name[0] != '/')
+         snprintf(path, sizeof(path), MODULEDIR "/%s", name);
+      else
+         snprintf(path, sizeof(path), "%s", name);
+
+      module = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
+      if (!module) {
+         fprintf(stderr, "failed to load module: %s\n", dlerror());
+      }
+      init = dlsym(module, entrypoint);
    }
 
-   return dlsym(module, entrypoint);
+   return init;
+}
+
+static const struct backend_desc *
+find_backend(const char *name)
+{
+   const struct backend_desc *backend = NULL;
+   unsigned i;
+
+   for (i = 0; i < ARRAY_SIZE(backends); ++i) {
+      if (strcmp(backends[i].name, name) == 0) {
+         backend = &backends[i];
+         break;
+      }
+   }
+
+   return backend;
 }
 
 struct gbm_device *
@@ -70,23 +99,23 @@ _gbm_create_device(int fd)
 {
    const struct gbm_backend *backend = NULL;
    struct gbm_device *dev = NULL;
-   int i;
+   unsigned i;
    const char *b;
 
    b = getenv("GBM_BACKEND");
    if (b)
-      backend = load_backend(b);
+      backend = load_backend(find_backend(b));
 
    if (backend)
       dev = backend->create_device(fd);
 
    for (i = 0; i < ARRAY_SIZE(backends) && dev == NULL; ++i) {
-      backend = load_backend(backends[i]);
+      backend = load_backend(&backends[i]);
       if (backend == NULL)
          continue;
-      fprintf(stderr, "loaded module: %s\n", backends[i]);
+
       dev = backend->create_device(fd);
    }
-   
+
    return dev;
 }
